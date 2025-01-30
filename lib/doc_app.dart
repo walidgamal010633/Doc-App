55dac480269ec991eec8be6362e700fc45d43557
walid gamal <walidgamal010633/Doc-App.git>
2025-01-30 19:29:08 +0200

    first commit

---
+++ lib/doc_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Doc_App extends StatelessWidget {
+  const Doc_App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:docapp/doc_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Doc_App());
+}
